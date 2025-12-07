7a44ddadacdccec3127f29dea7408ffb0350af30
abhaytiwaryi <abhayraj702unique@gmail.com>
2025-12-07 09:48:03 +0530

    star

--- nPattern.cpp
+++ nPattern.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 int main(){
-
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            cout<<"@"<<" ";
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*"<<" ";
         }cout<<endl;
     }
 }
