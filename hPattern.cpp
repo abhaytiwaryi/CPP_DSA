c5b24d11def07da567c0eee53f829d6fd2353340
abhaytiwaryi <abhayraj702unique@gmail.com>
2025-12-10 13:48:29 +0530

    hollow pattenr

---
+++ hPattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int r,c;
+    cout<<"Input row: ";
+    cin>>r;
+    cout<<"Input comlumn: ";
+    cin>>c;
+
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+            if(i==1 || j==1 || i==r || j==c){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }cout<<endl;
+    }
+}
